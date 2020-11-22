96db32b86c692620344c2b4ceb80e073614173a2
mpragnarok <mp922352612@gmail.com>
2020-11-22 09:59:03 +0800

    <Init project><router><login, dashboard, users and components wrapper, nav, menu>

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Dashboard from "./secure/Dashboard";
+import Users from "./secure/Users";
+import Login from "./public/Login";
+import { BrowserRouter, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Route path={"/"} exact component={Dashboard} />
+                <Route path={"/users"} component={Users} />
+                <Route path={"/login"} component={Login} />
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
